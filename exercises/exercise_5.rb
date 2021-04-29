aef8cf81fcb090a084ee429c429c2489f9b3b335
Eiman <imansawi>
2021-04-29 00:43:49 -0400

    add exercises files to practice CRUD operations and data validation using AR

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@sum = Store.sum(:annual_revenue)
+@averag = Store.average(:annual_revenue)
+
+@millions = Store.where("annual_revenue > 1000000").count
+
+puts @sum
+puts @averag
+puts @millions
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,8 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Joe", last_name: "John", hourly_rate: 50)
+@store2.employees.create(first_name: "Mark", last_name: "Paul", hourly_rate: 40)
+@store2.employees.create(first_name: "Cecil", last_name: "Larry", hourly_rate: 57)
+@store1.employees.create(first_name: "Sara", last_name: "Roy", hourly_rate: 100)
